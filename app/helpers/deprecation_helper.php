b45add8e8acaf921f0b126110831d39708be3422
Sander Muller <github@scode.nl>
2024-12-02 13:53:47 +0100

    Reproduce Concurrency exception

---
+++ app/helpers/deprecation_helper.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+if (! function_exists('deprecation_causer')) {
+    function deprecation_causer(string $key = null, $value = null): void
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Concurrency;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    Concurrency::run([
+        fn () => null,
+        fn () => null,
+    ]);
+
     return view('welcome');
 });
